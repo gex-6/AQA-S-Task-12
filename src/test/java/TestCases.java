aa8317ad1a7e98dc78c4e6280b6023bd1267159e
Taras Stepaniuk <taras.stepaniuk@gmail.com>
2024-01-27 12:23:07 +0200

    refactored a bit

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -9,14 +9,14 @@ public class TestCases {
     String bookName = "Про Миколу Гоголя, Марка Твена, Ніколу Теслу, Альберта Ейнштейна, Стівена Кінга"; //not available
     String bookName2 = "Знову й знову"; //available both
     String bookName3 = "Ідентичність"; //available in paper
-
     String url = "https://www.yakaboo.ua/";
-
     public SelenideElement searchField = Selenide.$("[type=\"search\"]");
     public SelenideElement searchButton = Selenide.$("[class=\"ui-search-form-input\"]>button[class=\"ui-btn-primary\"]");
     public SelenideElement noBookButton = Selenide.$("[class=\"base-product__buying\"] ~ [class=\"ui-btn-accept waiting-list button-accept\"]");
     public SelenideElement paperBookButton = Selenide.$("[class=\"paper\"]");
     public SelenideElement digitalBookButton = Selenide.$("[class=\"digital\"]");
+    public SelenideElement paperPrice = Selenide.$("div.ui-price-display.price.simple.selected > div.ui-price-display__main.special-price > span");
+    public SelenideElement digitalPrice = Selenide.$("div.ui-price-display.price.simple:last-of-type > [class=\"ui-price-display__main\"] > span");
 
     @Test
     public void yakabooTests() throws InterruptedException {
@@ -34,12 +34,12 @@ public class TestCases {
         } else {
             Thread.sleep(1000);
             if (paperBookButton.isDisplayed()) {
-                String pricePaper = Selenide.$("div.ui-price-display.price.simple.selected > div.ui-price-display__main.special-price > span").getText();
+                String pricePaper = paperPrice.getText();
                 System.out.println("Ціна паперової книги: " + pricePaper);
             }
             Thread.sleep(1000);
             if (digitalBookButton.isDisplayed()) {
-                String priceDig = Selenide.$("div.ui-price-display.price.simple:last-of-type > [class=\"ui-price-display__main\"] > span").getText();
+                String priceDig = digitalPrice.getText();
                 System.out.println("Ціна електронної книги: " + priceDig);
             }
         }
